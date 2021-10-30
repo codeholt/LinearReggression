81f429c62df3a061f652c49d7f2d4b59f51c7239
Cody Overholt <cjmatek@gmail.com>
2021-10-30 17:07:27 -0600

    midterm analysis done

--- Week 7/MidtermAnalysisExploration.R
+++ Week 7/MidtermAnalysisExploration.R
@@ -4,10 +4,7 @@ library(car)
 
 midata <- read.csv("Math425PastGrades.csv", stringsAsFactors = TRUE)
 View(midata)
-#magic2groups
-# group 2 is hunch of strong math background
-# group 1 is hunch on weak math background
-# plan to compensate: make predictions from both.
+
 
 
 #checking varable interaction pairs
@@ -52,7 +49,7 @@ pairs(midata2[,-3], panel = panel.smooth)
 #Columns to maybe disclude from the model
 #Section, time in office hours
 
-#amaking a plot to view midterm, final, and total exam scores
+#making a plot to view midterm, final, and total exam scores
 ggplot(data=midata2)+
   geom_point(aes(x=Math.425.Midterm, y=(.7*Final.Exam + .3*Math.425.Midterm)))+
   geom_point(aes(x=Math.425.Midterm, y=Final.Exam), color = "red")
@@ -90,7 +87,8 @@ summary(mylm)
 ggplot(data=midata2,aes(x=Math.425.Midterm, y=Final.Exam, color=as.factor(MagicTwoGroups)))+
   geom_point()+
   labs(x="Midterm Score (%)", y="Final")+
-  geom_hline(yintercept = 0)
+  geom_smooth(method = 'lm')
+  
 
 ######Notes
 #simple linear regression has the midterm be significant, but the R^2 is low. using the pairs I can probably do better
@@ -103,17 +101,56 @@ ggplot(data=midata2,aes(x=Math.425.Midterm, y=Final.Exam, color=as.factor(MagicT
 #R^2 of .3984 with lm(Final.Exam~Math.425.Midterm + Math.425.Midterm:Assessment.Quizzes.Final.Score +  Math.425.Midterm:AttendedAlmostAlways, data= midata2)
 #will need to ask what variables we can use and if we know them
 #magic 2 seems to be the lynch pin. what is it, and what would mine be?
+#magic2groups
+# group 2 is hunch of strong math background
+# group 1 is hunch on weak math background
+# plan to compensate: make predictions from both.
 
 #Step 2 calculate prediction intervals for final exam score 100% final or 70% final 30% midterm
+pred <- predict(mylm, data.frame(Math.425.Midterm = c(64,64), MagicTwoGroups = c(1,2)), interval = 'predict')
+predict(mylm, data.frame(Math.425.Midterm = 64, MagicTwoGroups = 1, interval = 'predict'))
 
+ggplot(data=midata2,aes(x=Math.425.Midterm, y=Final.Exam, color=as.factor(MagicTwoGroups)))+
+  geom_point()+
+  scale_color_manual(values = c("steelblue", "firebrick"),
+                     name="MagicTwoGroups")+
+  labs(x="Midterm Score (%)", y="Final")+
+  geom_smooth(method = 'lm')+
+  #magic group 1
+  geom_segment(aes(x=64, xend=64, y=pred[4],yend=pred[6]),size = 4, alpha = .01)+
+  geom_point(aes(x=64, y=pred[1]),size = 3, color = 'black')+
+  #magic group 2
+  geom_segment(aes(x=64, xend=64, y=pred[3],yend=pred[5]),size = 4, alpha = .01, color = "steelblue")+
+  geom_point(aes(x=64, y=pred[2]),size = 3, color = 'black')+
+  theme_bw()+
+  geom_vline(xintercept = 64)
+
+
+#compare grade predictions
+ggplot(data=midata2)+
+  scale_color_manual(values = c("steelblue", "firebrick"),
+                     name="MagicTwoGroups")+
+  geom_point(aes(x=Math.425.Midterm, y=(.7*Final.Exam + .3*Math.425.Midterm)-Final.Exam, color=as.factor(MagicTwoGroups)))+
+  #group 1
+  geom_point(aes(x=64, y=(.7*pred[1] + .3*64)-pred[1]), color = "steelblue", size = 4)+
+  geom_segment(aes(x=64, xend=64, y=(.7*pred[3] + .3*64)-pred[3],yend=(.7*pred[5] + .3*64)-pred[5]),size = 4, alpha = .01, color = "steelblue")+
+  #group 2
+  geom_point(aes(x=64, y=(.7*pred[2] + .3*64)-pred[2]), color = "firebrick", size = 4)+
+  geom_segment(aes(x=64, xend=64, y=(.7*pred[4] + .3*64)-pred[4],yend=(.7*pred[6] + .3*64)-pred[6]),size = 4, alpha = .01, color = "firebrick")+
+  labs(x="Midterm Score (%)", y="Difference in Exam total if midterm is dropped ((Final+Midterm)-Final)",title = "Final Exam grade Difference")+
+  geom_hline(yintercept = 0)+
+  theme_bw()+
+  annotate(x=50,y=-5, label="Better to drop midterm", geom="text")+
+  annotate(x=50,y=10, label="Better to keep midterm", geom="text")
 #Step 3 decide
 
+#it seems better to keep the midterm, as unhappy with my grade that I am
 
 
 
 
-
-
+plot(mylm, which = 1:2)
+plot(mylm$residuals)
 
 
 
