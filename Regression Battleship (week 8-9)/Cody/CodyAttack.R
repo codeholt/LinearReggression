96c59320c037456860d337f6e5206f1989bb5167
Cody Overholt <cjmatek@gmail.com>
2021-11-11 16:12:04 -0700

    Regression battleship. I surrender

--- Regression Battleship (week 8-9)/Cody/CodyAttack.R
+++ Regression Battleship (week 8-9)/Cody/CodyAttack.R
@@ -3,14 +3,17 @@ library(car)
 library(mosaic)
 
 rbdata2 <- read.csv("rbdata.csv")
-
+View(rbdata2)
 
 #model
-mylm2 <- lm(y~x7+
-              x3+x3:x7+
-              x6+x6:x7,data=rbdata2)
+mylm2 <- lm(y~x7+I(x7^2)+I(x7^3)+
+              x3+x3:x7+I(x7^2):x3+I(x7^3):x3
+              ,data=rbdata2)
 summary(mylm2)
-pairs(cbind(R=mylm2$residuals,fit=mylm2$fitted.values,rbdata2),pch=16, panel=panel.smooth)
+b <- coef(mylm2)
+pairs(cbind(R=mylm2$residuals,fit=mylm2$fitted.values,rbdata2),
+      pch=16, panel=panel.smooth,
+      col = interaction(rbdata2$x3))
 #no x or y transformations
 #or x8
 #is x7
@@ -19,8 +22,16 @@ pairs(cbind(R=mylm2$residuals,fit=mylm2$fitted.values,rbdata2),pch=16, panel=pan
 #no more than 2 on off switches
 #neither x9 or x10
 
-
+table(rbdata2$x10)
 
 
 #x10 looks promising
 #x3 holds some significance
+palette(c("skyblue","skyblue4","orange","orangered", "firebrick", "firebrick4", "green", "limegreen"))
+rbdata2 %>% 
+  ggplot(aes(x=x7,y=y, color = as.factor(x3))) +
+  scale_fill_manual(values = palette())+
+  scale_color_manual(values = palette())+
+  geom_point()+
+  stat_function(fun=function(x) b[1]+b[2]*x+b[3]*x^2+b[4]*x^3, color = "skyblue")+
+  stat_function(fun=function(x) (b[1]+b[5])+(b[2]+b[6])*x+(b[3]+b[7])*x^2+(b[4]+b[8])*x^3)
--- Regression Battleship (week 8-9)/David/davidAttack.R
+++ Regression Battleship (week 8-9)/David/davidAttack.R
@@ -5,13 +5,18 @@ rbdata1 <- read.csv("rbdata.csv")
 
 
 #model
-mylm1 <- lm(y~x7+
-              x6+x6:x7+
-              x5:I(x7^3)+
-              x2+x2:x7,data=rbdata1)
+mylm1 <- lm(y~x7+I(x7^2)+I(x7^3)+
+              x3+x3:x7+x3:I(x7^2)+I(x7^3):x3+
+              
+              x10:x3:x7
+              ,data=rbdata1)
+
+#+x9:x7+x9:I(x7^2)
 summary(mylm1)
+b <- coef(mylm1)
 pairs(cbind(R=mylm1$residuals,fit=mylm1$fitted.values,rbdata1),pch=16, panel=panel.smooth,
-      col = interaction(rbdata1$x6,rbdata1$x2))
+      col = interaction(rbdata1$x3,rbdata1$x10))
+table(rbdata1$x9)
 #x7:x6 showed some significance
 #uses polynomial x variable transformations
 #no y transformations
@@ -20,5 +25,19 @@ pairs(cbind(R=mylm1$residuals,fit=mylm1$fitted.values,rbdata1),pch=16, panel=pan
 #x9 is an on off switch
 
 rbdata1 %>% 
-  ggplot(aes(x=x7,y=y,color=interaction(x6,x5,x2)))+
-  geom_point()
+  ggplot(aes(x=x7,y=y, color = interaction(x3,x10)))+
+  scale_fill_manual(values = palette())+
+  scale_color_manual(values = palette())+
+  geom_point()+
+  #geom_smooth(method='lm', se=F, formula = y~poly(x,3))
+  #x3=0,x10=0
+  stat_function(fun=function(x) b[1]+b[2]*x+b[3]*x^2+b[4]*x^3, color = palette()[1])+
+  #x3=1,x10=0
+  stat_function(fun=function(x) (b[1]+b[5])+(b[2]+b[6])*x+(b[3]+b[7])*x^2+(b[4]+b[8])*x^3,color = palette()[2])+
+  #x3=0,x10=1
+  stat_function(fun=function(x) b[1]+(b[2]+b[9])*x+b[3]*x^2+b[4]*x^3, color = palette()[3])+
+  #x3=1,x10=1
+  stat_function(fun=function(x) (b[1]+b[5])+(b[2]+b[6]+b[9])*x+(b[3]+b[7])*x^2+(b[4]+b[8])*x^3,color = palette()[4])
+
+
+palette(c("green", "limegreen","skyblue4","orange","orangered", "firebrick", "firebrick4","skyblue"))
