92fcbf1180d4dc4ac1c047f225640a290802d42d
Cody Overholt <cjmatek@gmail.com>
2021-11-18 20:34:44 -0700

    .83 R^2 push

--- Week 10/house-prices-advanced-regression-techniques-1/HousingAnalysisExplore.R
+++ Week 10/house-prices-advanced-regression-techniques-1/HousingAnalysisExplore.R
@@ -15,18 +15,44 @@ mytrain1 <- train %>%
            MSZoning %in% c("RH","RL","RM","RP")~"R", 
            TRUE~MSZoning)) , 
          Alley = as.character(Alley),
-         Alley = as.factor(replace_na(as.character(Alley), "NoAccess")),
+         Alley = as.factor(replace_na(Alley, "NoAccess")),
          #changes them back
          MSZoning = as.factor(MSZoning),
          Alley = as.factor(Alley))
 
 
-lm1 <- lm(SalePrice~TotalSF+Alley+MSZoning2, data=mytrain1)
+lm1 <- lm(SalePrice~TotalSF+I(TotalSF^2)+OverallQual+SaleCondition+Neighborhood, data=mytrain1)
 summary(lm1)
 
 plot(SalePrice~TotalSF, data = mytrain1)
 
 plot(lm1$residuals~ . , data= mytrain1)
+
+
+#Looking for "Those two fuckers"
+#built post 2000
+#RoofStyle = "Hip"
+#RoofMatl = ClyTile?
+#MasVnrType = Stone
+#Fireplace Qty
+#FinishGarage
+#New Houses for some reason
+#SaleCondition (The two in question were not complete when sold)
+
+#residuals to look into
 #MSSubCLass
 #MSzoning
+#Neighborhood
+#condition2
+#houseStyle
+#overallQual
+#RoofMatl
+#Exterior 2nd
+#ExterQual
+#foundation
+#Has a basement vs not?
+#heating
+#has Second Floor?
+
+
 
