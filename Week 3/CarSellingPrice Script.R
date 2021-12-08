83bef543195e7d0666d7d191f8fa3525d2265689
Cody Laptop <cjmatek@gmail.com>
2021-12-08 15:54:33 -0700

    classwork wednesday

--- Week 3/CarSellingPrice Script.R
+++ Week 3/CarSellingPrice Script.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(car)
 
-carPrice <- read_csv("C:/Users/Cody/Documents/College/10th semester (fall 2021)/Math 425 (linear Regression)/Cars Data (GLS VW passat).csv")
+carPrice <- read_csv("Cars Data (GLS VW passat).csv")
 View(carPrice)
 rows <- c(1:55,57:58)
 carPrice <- carPrice[rows,] 
@@ -28,14 +28,20 @@ ggplot(data=carPrice, aes(x=Mileage,y=Price))+
   #geom_smooth(method='lm', se=FALSE)+
   stat_function(fun=function(x) exp(bLog[1]+bLog[2]*x), color='black')+
   geom_point(aes(x=78200, y=9141), color="red", size=2)+
+  geom_point(aes(x=200000,y=sell[2]), color = "red", size = 2)+
   labs(title="VW Passat Sale Value Per Mile Driven")+
+  geom_text(aes(x=140000, y=10000, label = "Bought car at 78200 Miles"))+
+  geom_text(aes(x=260000, y=sell[2]+1000, label = "sell car at 200000 Miles"))+
   theme_bw()
 
-predict(priceLog.lm, data.frame(Mileage=c(91000, 200000))) %>% exp()
+sell <- predict(priceLog.lm, data.frame(Mileage=c(91000, 200000))) %>% exp()
 predict(priceLog.lm, data.frame(Mileage=c(78200, 150000))) %>% 
   exp()
 
+#
+
 mileageSeq <- c(78200, seq( 80000, 200000, by = 5000))
+
 sale <- predict(priceLog.lm, data.frame(Mileage=mileageSeq)) %>% 
   exp()
 
